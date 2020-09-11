dac06ee4ef55a243f30e278f7437ca11643887e6
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2020-09-10 21:59:54 -0300

    Component callable type_traverse error

--- c_drivers/python_vm.c
+++ c_drivers/python_vm.c
@@ -1,56 +1,60 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <stdio.h>
-
+#include <structmember.h>
 #define PY_NONE Py_BuildValue("")
 
+#include <time.h>
 
 /* ########## FIRST VERSION ########## */
-void C_run_python_file(const char* script) {
-    printf("`python_vm.c`: Starting python\n");
-    wchar_t *program = Py_DecodeLocale("", NULL);
-    if (program == NULL) {
-        fprintf(stderr, "Fatal error: cannot decode arg[0]\n");
-        exit(1);
-    }
-    Py_SetProgramName(program);
+/* void C_run_python_file(const char* script) { */
+/*     printf("`python_vm.c`: Starting python\n"); */
+/*     wchar_t *program = Py_DecodeLocale("", NULL); */
+/*     if (program == NULL) { */
+/*         fprintf(stderr, "Fatal error: cannot decode arg[0]\n"); */
+/*         exit(1); */
+/*     } */
+/*     Py_SetProgramName(program); */
    
-    // the python vm should be initialized before this function
-    Py_Initialize();
+/*     // the python vm should be initialized before this function */
+/*     Py_Initialize(); */
     
-    FILE *script_f = fopen(script, "r");
-    PyRun_SimpleFile(script_f, script);
+/*     FILE *script_f = fopen(script, "r"); */
+/*     PyRun_SimpleFile(script_f, script); */
 
-    if(Py_FinalizeEx < 0) {
-        exit(120);
-    }
-    PyMem_RawFree(program);
-    printf("`python_vm.c`: Finalizing python\n");
-}
+/*     if(Py_FinalizeEx < 0) { */
+/*         exit(120); */
+/*     } */
+/*     PyMem_RawFree(program); */
+/*     printf("`python_vm.c`: Finalizing python\n"); */
+/* } */
 
 typedef struct {
     PyObject_HEAD
+    PyObject* type_id;
 } MetaComponentObject;
 
-/* static PyObject *MetaComponent_call(MetaComponentObject *type, PyObject *args, PyObject *kwargs) { */
-/*     PyObject *obj = PyType_GenericNew((PyTypeObject *) type, args, kwargs); */
+static PyObject* *MetaComponent_call(PyObject *cls, PyObject *args, PyObject *kwargs) {
+    fprintf(stderr, "Creating class instance\n");
+    
+    PyObject *obj = PyObject_CallMethodObjArgs(cls, PyUnicode_FromString("__new__"), cls, args, kwargs);
+    /* MetaComponentObject *obj = PyType_GenericNew(self, args, kwargs); */
+    fprintf(stderr, "LOG\n");
+    /* obj->type_id = PyLong_FromLong(random()); */
+    
+    PyObject *class_instance = PyObject_CallMethodObjArgs(obj, PyUnicode_FromString("__init__"), args, kwargs);
 
-/*     PyObject *dict = MetaComponentType->tp_dict; */
-/*     PyObject *type_id = PyLong_FromLong(666); */
-/*     PyDict_SetItemString(dict, "__type_id__", type_id); */
+    fprintf(stderr, "LOG OUT\n");
+    return class_instance;
+}
 
-/*     return obj; */
-/* } */
+static PyObject *MetaComponent_new(MetaComponentObject *cls, PyObject *args, PyObject *kwargs) {
+    MetaComponentObject *obj = PyType_GenericNew(cls, args, kwargs);
+    fprintf(stderr, "new chamado\n");
+    obj->type_id = PyLong_FromLong(random());
 
-/* static PyTypeObject MetaComponentType = { */
-/*     PyVarObject_HEAD_INIT(NULL, 0) */
-/*     .tp_name = "engine.MetaComponent", */
-/*     .tp_basicsize = sizeof(MetaComponentObject), */
-/*     .tp_itemsize = 0, */
-/*     .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, */
-/*     .tp_new = PyType_GenericNew, */
-/*     .tp_call = (ternaryfunc) MetaComponent_call, */
-/* }; */
+    return obj;
+}
 
 static PyObject* say_hello(PyObject *self, PyObject *args) {
     PyObject *p;
@@ -65,6 +69,23 @@ static PyMethodDef EngineMethods[] = {
     {NULL, NULL, 0, NULL}
 };
 
+static PyMemberDef MetaComponent_members[] = {
+    {"__type_id__", T_OBJECT_EX, offsetof(MetaComponentObject, type_id), 0, "type id"},
+    {NULL}
+};
+
+static PyTypeObject MetaComponentType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "engine.MetaComponent",
+    .tp_basicsize = sizeof(MetaComponentObject),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    /* .tp_new = PyType_GenericNew, */
+    .tp_new = (ternaryfunc) MetaComponent_new,
+    /* .tp_call = (ternaryfunc) MetaComponent_call, */
+    .tp_members = MetaComponent_members,
+};
+
 static PyModuleDef EngineModule = {
     PyModuleDef_HEAD_INIT, "engine", NULL, -1, EngineMethods,
     NULL, NULL, NULL, NULL
@@ -73,7 +94,20 @@ static PyModuleDef EngineModule = {
 static PyObject*
 PyInit_engine(void)
 {
-    return PyModule_Create(&EngineModule);
+    PyObject* module = PyModule_Create(&EngineModule);
+    /* Py_INCREF(&PyType_Type); */
+    MetaComponentType.tp_base = &PyType_Type;
+    
+    if (PyType_Ready(&MetaComponentType) < 0)
+        printf("error\n");
+        /* return; */
+
+    Py_INCREF(&MetaComponentType);
+    if(PyModule_AddObject(module, "MetaComponent", &MetaComponentType) < 0) {
+        printf("Error adding component\n");
+    }
+
+    return module;
 }
 
 void C_RUN_PYSCRIPT(const char* script) {
--- examples/python/hello.py
+++ examples/python/hello.py
@@ -1,4 +1,25 @@
 import engine
 
-engine.say_hello()
-engine.say_hello()
+# engine.say_hello()
+# print(f'Engine module: {dir(engine)}')
+# print(f'MetaComponent type: {type(engine.MetaComponent)}')
+# print(f'MetaComponent dir: {dir(engine.MetaComponent)}')
+
+class Color():
+    __metaclass__ = engine.MetaComponent   
+    def __init__(self, r, g, b):
+        print("calling init")
+
+class Transform(metaclass=engine.MetaComponent):
+    pass
+
+print(f'Color dir: {dir(Color)}')
+# print(f'Component type: {type(Component)}')
+# print(dir(Component))
+# print(dir(Color))
+# print(f'Color type_id: {Color.__type_id__}')
+# print(f'Transform type_id: {Transform.__type_id__}')
+
+c = Color(1,2,3)
+# print(dir(c))
+
