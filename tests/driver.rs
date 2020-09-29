37923ee89eeb91ecdaef4b8d652a108bbe542196
Pablo Diego <pablodiegoss@hotmail.com>
2020-09-28 22:21:11 -0300

    Fix old test that runs a python script

--- tests/driver.rs
+++ tests/driver.rs
@@ -2,8 +2,9 @@ use legion_script::*;
 
 #[test]
 fn run_python_script() {
+    let mut id_count = 0_u64;
     driver::run_script(
-        String::from("/home/shammyz/Documents/repos/legion_script/tests/scripts/print.py")
-    );
+        String::from("tests/scripts/print.py"), &mut id_count);
     assert_eq!(0,0);
 }
+
