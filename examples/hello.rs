440d28e5ee1ea1976ae33e6547fb0be383ecc3a8
Pablo Diego <pablodiegoss@hotmail.com>
2020-10-18 17:53:42 -0300

    Add function to return scripting system's closure

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,15 +1,9 @@
 use legion::*;
 
 use legion_script::{
-    system::{/*local_scripting_system, */Scripts, ComponentId},
-    query::{get_external_components_ids},
-    utils::{create_test_component_data},
-    components::{Position, Rotation},
-    query::{get_external_components},
-    driver::run_script,
+    system::{create_scripting_system, ComponentId},
 };
 
-use std::os::raw::c_void;
 use simple_logger::{SimpleLogger};
 use log::*;
 
@@ -25,18 +19,17 @@ pub fn main() {
     let mut world = World::default();
     let mut resources = Resources::default();
     
-    let id_count = 0;
-    resources.insert::<ComponentId>(id_count);
-    
     let scripts = vec![String::from("examples/python/hello.py"), String::from("examples/python/hello2.py")];
-
-    let mut schedule = Schedule::builder()
-        .add_thread_local_fn(move |_world, _resources|{
-            debug!("World len do system {}", _world.len());
-            run_script(_world, scripts[0].clone(), &mut 1);
-        // run_script(_world, scripts[1].clone(), &mut 1);
-        })
-        .build();
-
-    schedule.execute(&mut world, &mut resources);
+    
+    static mut ID_COUNT: u64 = 0;
+    unsafe{
+        resources.insert::<ComponentId>(ID_COUNT);
+            
+        let mut schedule = Schedule::builder()
+            .add_thread_local_fn(create_scripting_system(scripts, &mut ID_COUNT))
+            .build();   
+        
+        schedule.execute(&mut world, &mut resources);
+    }
 }
+
--- src/system.rs
+++ src/system.rs
@@ -4,6 +4,16 @@ use crate::driver::run_script;
 pub type ComponentId = u64;
 pub type Scripts = Vec<String>;
 
+use log::*;
+
+pub fn create_scripting_system(scripts: Vec<String>, id_count: &mut u64)->Box<dyn FnMut(&mut legion::World, &mut legion::Resources) + '_>{
+    Box::new(move |_world: &mut legion::World, _resources|{
+        debug!("World len do system {}", _world.len());
+        crate::driver::run_script(_world, scripts[0].clone(), id_count);
+        // run_script(_world, scripts[1].clone(), &mut 1);
+    })
+}
+
 // #[system]
 // pub fn scripting(#[resource] scripts: &mut Scripts, #[resource] component_id: &mut ComponentId) {
 //     run_script(scripts[0].clone(), component_id);
