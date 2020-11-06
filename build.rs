97152c9458ac30d7b27b502b945176e2fc4dd8ca
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2020-11-06 14:09:08 -0300

    Update build.rs

--- build.rs
+++ build.rs
@@ -12,21 +12,23 @@ fn main() {
     .output()
     .expect("failed to execute process");
     
+    let ldflags = Command::new("python3.7-config")
+    .arg("--ldflags")
+    .output()
+    .expect("failed to execute process");
+    
+    eprintln!("{}", String::from_utf8_lossy(&ldflags.stdout));
     
     let python_includes = String::from_utf8_lossy(&output.stdout);
     let formatted_python_includes = String::from(python_includes).trim_end().replace("-I", "");
-    eprintln!("argghhhh {}",formatted_python_includes);
+    eprintln!("{}",formatted_python_includes);
     let formatted_python_includes = String::from(formatted_python_includes.split_whitespace().next().unwrap());
 
     cc::Build::new()
         .include(formatted_python_includes)
-        .flag("-lpython3.7")
+        .flag("-lpython3.7m")
         .file("c_drivers/python_vm.c")
         .compile("python_vm");
 
-    // cbindgen::generate(&crate_dir)
-        // .expect("cbindgen: Failed to generate binds")
-        // .write_to_file("gen/ecs.h");
-
-    println!("cargo:rustc-flags=-l python3.7m");
+    println!("{}", format!("cargo:rustc-flags={}", String::from_utf8_lossy(&ldflags.stdout)));
 }
