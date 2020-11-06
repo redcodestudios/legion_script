8f816345a84f54ab223f081235b7c2deb8f8a4f3
Pablo Diego Silva da Silva <pablodiegoss@hotmail.com>
2020-11-05 22:41:10 -0300

    Create rust.yml

--- build.rs
+++ build.rs
@@ -2,15 +2,25 @@ extern crate cc;
 extern crate cbindgen;
 
 use std::env;
+use std::process::Command;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").expect("Cargo manifest not found");
-
-    let python_includes = "/usr/include/python3.7";
+    
+    let output = Command::new("python3.7-config")
+    .arg("--includes")
+    .output()
+    .expect("failed to execute process");
+    
+    
+    let python_includes = String::from_utf8_lossy(&output.stdout);
+    let formatted_python_includes = String::from(python_includes).trim_end().replace("-I", "");
+    eprintln!("argghhhh {}",formatted_python_includes);
+    let formatted_python_includes = String::from(formatted_python_includes.split_whitespace().next().unwrap());
 
     cc::Build::new()
-        .include(python_includes)
-        .flag("-lpython3.7m")
+        .include(formatted_python_includes)
+        .flag("-lpython3.7")
         .file("c_drivers/python_vm.c")
         .compile("python_vm");
 
